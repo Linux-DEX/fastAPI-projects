dd63887d907d106d7346f4d673dc7b5de13c34d6
Linux-DEX <sarabjeet.9353@gmail.com>
2024-07-28 10:23:07 +0530

    name change for FARM_project

---
+++ FARM_project/frontend/src/components/TodoListView.jsx
@@ -0,0 +1,11 @@
+import TodoItem from "./Todo";
+
+export default function TodoView(props) {
+    return (
+        <div>
+            <ul>
+                {props.todoList.map((todo, index) => <TodoItem key={index} todo={todo} />)}
+            </ul>
+        </div>
+    )
+}
